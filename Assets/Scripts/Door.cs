ad6d939db2c0e6422e00f95ccdfb6b4a542ad800
Nikita Korneev <kornniki4@yandex.ru>
2026-03-31 02:07:41 +0300

    asdd

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,25 +5,46 @@ public class Door : MonoBehaviour
     private Collider2D col;
     private SpriteRenderer sr;
 
+    [Header("Внешний вид двери")]
+    [SerializeField] private Sprite openSprite;
+
+    private Sprite closedSprite;
+
     public bool IsOpen { get; private set; }
 
     private void Awake()
     {
         col = GetComponent<Collider2D>();
         sr = GetComponent<SpriteRenderer>();
+
+        if (sr != null)
+            closedSprite = sr.sprite;
+
+        if (col != null)
+            col.isTrigger = false;
     }
 
     public void Open()
     {
         IsOpen = true;
-        if (col != null) col.enabled = false;
-        if (sr != null) sr.enabled = false;
+
+        // Открытая дверь больше не блокирует игрока, но остаётся видимой
+        // с отдельным спрайтом и работает как вход на следующий уровень.
+        if (col != null)
+            col.isTrigger = true;
+
+        if (sr != null && openSprite != null)
+            sr.sprite = openSprite;
     }
 
     public void Close()
     {
         IsOpen = false;
-        if (col != null) col.enabled = true;
-        if (sr != null) sr.enabled = true;
+
+        if (col != null)
+            col.isTrigger = false;
+
+        if (sr != null && closedSprite != null)
+            sr.sprite = closedSprite;
     }
 }
